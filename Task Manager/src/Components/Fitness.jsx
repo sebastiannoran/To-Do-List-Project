a745455006d7c022cc0ec3bdd4219414f9a393c2
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-02 19:46:26 -0400

    New JSON file + fitness component

---
+++ Task Manager/src/Components/Fitness.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FitnessButton = () => {
+  return (
+    <div>
+      <h2>Fitness Tasks</h2>
+      {/* Add any specific content or functionality for the Fitness button */}
+    </div>
+  );
+};
+
+export default FitnessButton;
