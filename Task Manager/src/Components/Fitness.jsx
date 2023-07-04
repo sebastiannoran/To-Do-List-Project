6b7cf248db418a3bd3e879584d591725dda808f1
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-04 13:38:42 -0400

    Main works, make changes to branches

--- Task Manager/src/Components/Fitness.jsx
+++ Task Manager/src/Components/Fitness.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-const FitnessButton = () => {
-  return (
-    <div>
-      <h2>Fitness Tasks</h2>
-      {/* Add any specific content or functionality for the Fitness button */}
-    </div>
-  );
-};
+// const FitnessButton = () => {
+//   return (
+//     <div>
+//       <h2>Fitness Tasks</h2>
+//       {/* Add any specific content or functionality for the Fitness button */}
+//     </div>
+//   );
+// };
 
-export default FitnessButton;
+// export default FitnessButton;
