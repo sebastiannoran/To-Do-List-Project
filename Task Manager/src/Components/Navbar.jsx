ab4ff0b2f6e52282f494d348a5e55fecc81ad908
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-06 10:08:37 -0400

    final changes, no more mystery box

--- Task Manager/src/Components/Navbar.jsx
+++ Task Manager/src/Components/Navbar.jsx
@@ -7,13 +7,6 @@ const Navbar = () => {
         <div className="container">
           <Link className="navbar-brand" to="/">
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
